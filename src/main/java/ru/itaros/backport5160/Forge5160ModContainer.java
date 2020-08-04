a362d609ca05d10d23d6948a7d52c033617f7a3c
makamys <makamys@outlook.com>
2020-08-04 21:31:21 +0200

    Add myself to credits

--- src/main/java/ru/itaros/backport5160/Forge5160ModContainer.java
+++ src/main/java/ru/itaros/backport5160/Forge5160ModContainer.java
@@ -22,7 +22,7 @@ public class Forge5160ModContainer extends DummyModContainer {
                 "Made specifically for fan of railcarts(Kane Hart) and lovers of pain(GTNH)";
         meta.version = "1.7.10";
         meta.authorList = Collections.singletonList("Itaros");
-        meta.credits = "gabizou, aikar, liach, williewillus, LexManos, tterrag1098, bs2609";
+        meta.credits = "gabizou, aikar, liach, williewillus, LexManos, tterrag1098, bs2609, makamys";
     }
 
     @Override
